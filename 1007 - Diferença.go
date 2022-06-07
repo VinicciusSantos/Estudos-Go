6f7dfdda93cab0d1b2fbc7ed1260fb1c71279dc5
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-06-07 09:05:34 -0300

    Create 1007 - Diferença.go

---
+++ 1007 - Diferença.go
@@ -0,0 +1,15 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+    var a, b, c, d int
+    fmt.Scanln(&a)
+    fmt.Scanln(&b)
+    fmt.Scanln(&c)
+    fmt.Scanln(&d)
+    
+    fmt.Printf("DIFERENCA = %v\n", a*b-c*d)
+}
